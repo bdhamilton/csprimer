8a5ac24972103a977d5233a06ad9fd961f6f5f35
Brian Hamilton <bdhamilton@gmail.com>
2026-02-26 19:32:28 -0500

    Get fully working beep beep

--- systems/beep-beep/beep.py
+++ systems/beep-beep/beep.py
@@ -8,15 +8,41 @@
 
 import sys
 from time import sleep
+import tty
+import termios
 
 def beep(n = 1):
   for _ in range(n):
+    # We need to write straight to the buffer to keep Python from trying
+    # to encode our byte value. b'\x07' is the ASCII code for BEL.
     sys.stdout.buffer.write(b'\x07')
-    sys.stdout.flush()
-    sleep(0.3)
 
-try:
-  n = int(sys.stdin.read())
-  beep(n)
-except:
-  pass
+    # Flush so the terminal receives BEL immediately, then pause between bells.
+    sys.stdout.flush()
+    sleep(0.2)
+
+def read_char():
+    # Get stdin's file descriptor and save its current settings.
+    current_stdin = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(current_stdin)
+
+    try:
+        # Raw mode disables line-buffered input, so read(1) returns after
+        # a single keypress instead of waiting for Enter.
+        tty.setraw(current_stdin)
+        ch = sys.stdin.read(1)
+    finally:
+        # Reset old settings after input has cleared to get out of raw mode.
+        termios.tcsetattr(current_stdin, termios.TCSADRAIN, old_settings)
+    return ch
+
+while True:
+    try:
+        n = read_char() 
+
+        if n == 'q':
+            break
+
+        beep(int(n))
+    except:
+        pass
